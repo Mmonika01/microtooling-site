4c378556d4d0bfffe0b841bb1f6537d8e4e6222c
Monika <83941446+Mmonika01@users.noreply.github.com>
2025-08-23 16:00:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert("Thanks for contacting Microtooling! We'll get back to you shortly.");
+});
